f3989e9e726f71c4dd7d67c75545681ec87fb700
Chris Muccioli <chris@kickstarter.com>
2014-09-11 01:19:56 -0400

    issue edits

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+
+	$('#div_demo').videoBG({
+		mp4:'images/christmas_snow.mp4',
+		ogv:'images/christmas_snow.ogv',
+		webm:'images/christmas_snow.webm',
+		poster:'images/christmas_snow.jpg',
+		scale:true,
+		zIndex:0
+	});
+
+})
